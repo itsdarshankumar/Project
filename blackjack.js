e03e857c2cc185e864b9a292ab62de07249b696e
Darshan Kumar <d_kumar@ch.iitr.ac.in>
2022-01-27 11:17:49 +0530

    Blackjack Main

---
+++ blackjack.js
@@ -0,0 +1,11 @@
+let card1 = document.getElementById("card1");
+let card2 = document.getElementById("card2");
+let card3 = document.getElementById("card3");
+let card4 = document.getElementById("card4");
+let card5 = document.getElementById("card5");
+let card6 = document.getElementById("card6");
+let card7 = document.getElementById("card7");
+let newcard = document.getElementById("push");
+let stand = document.getElementById("stand");
+
+newcard.addEventListener("click", function () {});
