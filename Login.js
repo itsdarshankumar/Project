af84820dc8962b11b13268e226f430a6f9801f24
Darshan Kumar <95537957+itsdarshankumar@users.noreply.github.com>
2022-01-30 16:42:27 +0530

    Delete Login.js

--- Login.js
+++
@@ -1,7 +0,0 @@
-function savetolocal2() {
-  localStorage.setItem(
-    "Userx",
-    JSON.stringify(document.getElementById("Username").value)
-  );
-}
-localStorage.setItem("totalcredit", "12345");
