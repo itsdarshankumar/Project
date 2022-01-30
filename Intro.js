8a1a99d2f808e045e85ff5ae0c70942fe0479e36
Darshan Kumar <95537957+itsdarshankumar@users.noreply.github.com>
2022-01-30 16:42:14 +0530

    Delete Intro.js

--- Intro.js
+++
@@ -1,4 +0,0 @@
-let x = JSON.parse(localStorage.getItem("Userx"));
-document.getElementById("user").textContent = "@" + x;
-document.getElementById("Totalcredit").textContent =
-  "Total credits= " + localStorage.getItem("totalcredit");
